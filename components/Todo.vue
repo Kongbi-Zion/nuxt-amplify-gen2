97495c3367b688aa14441299fe09242f787ba2f3
Kongbi-Zion <kongbizion3@gmail.com>
2024-04-27 13:29:45 +0100

    updates

--- components/Todo.vue
+++ components/Todo.vue
@@ -1,5 +1,48 @@
 <template>
-  <div></div>
+  <li class="flex items-center justify-between">
+    <div class="font-semibold border w-full rounded px-4 py-2.5">
+      {{ todo.content }}
+    </div>
+    <div class="min-w-[6rem]">
+      <button
+        type="submit"
+        class="inline-flex font-bold items-center py-2.5 px-3 ms-2 text-sm text-white rounded-lg border focus:outline-none bg-[#047D95]"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="w-4 h-4"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M5 19h1.425L16.2 9.225L14.775 7.8L5 17.575zm-2 2v-4.25L16.2 3.575q.3-.275.663-.425t.762-.15t.775.15t.65.45L20.425 5q.3.275.438.65T21 6.4q0 .4-.137.763t-.438.662L7.25 21zM19 6.4L17.6 5zm-3.525 2.125l-.7-.725L16.2 9.225z"
+          />
+        </svg>
+      </button>
+      <button
+        type="submit"
+        class="inline-flex font-bold items-center py-2.5 px-3 ms-1 text-sm text-white rounded-lg border focus:outline-none bg-red-500"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="w-4 h-4"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2zM18 4h-2.5l-.71-.71c-.18-.18-.44-.29-.7-.29H9.91c-.26 0-.52.11-.7.29L8.5 4H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1"
+          />
+        </svg>
+      </button>
+    </div>
+  </li>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps({
+  todo: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,57 @@
 <template>
-  <div class="h-[calc(100%-4rem)] w-full flex items-center justify-center">
+  <div class="h-[calc(100%-4rem)] w-full flex items-center justify-center px-6">
     <div class="mx-auto w-full max-w-screen-sm">
-      <div class="w-full p-5 border shadow-lg rounded-lg min-h-52">
+      <div class="w-full p-5 border shadow-lg rounded-lg min-h-40">
         <p class="text-center font-bold text-xl mb-6 mt-2">Todo List</p>
-        <div class="flex items-center max-w-lg mx-auto">
-          <div class="w-full">
-            <input
-              type="text"
-              id="add-todo"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full ps-4 p-2.5"
-              placeholder="Add todo..."
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            class="inline-flex font-bold items-center py-2.5 px-3 ms-2 text-sm text-white rounded-lg border focus:outline-none bg-[#047D95]"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="w-6 h-6"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="currentColor"
-                d="M11 13H6q-.425 0-.712-.288T5 12t.288-.712T6 11h5V6q0-.425.288-.712T12 5t.713.288T13 6v5h5q.425 0 .713.288T19 12t-.288.713T18 13h-5v5q0 .425-.288.713T12 19t-.712-.288T11 18z"
+
+        <p class="text-center">Opps please signIn!</p>
+
+        <div class="w-full max-w-lg mx-auto">
+          <div class="flex items-center">
+            <div class="w-full">
+              <input
+                type="text"
+                id="add-todo"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full ps-4 p-2.5"
+                placeholder="Add todo..."
+                required
               />
-            </svg>
+            </div>
+            <button
+              type="submit"
+              class="inline-flex font-bold items-center py-2.5 px-3 ms-2 text-sm text-white rounded-lg border focus:outline-none bg-[#047D95]"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="w-6 h-6"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M11 13H6q-.425 0-.712-.288T5 12t.288-.712T6 11h5V6q0-.425.288-.712T12 5t.713.288T13 6v5h5q.425 0 .713.288T19 12t-.288.713T18 13h-5v5q0 .425-.288.713T12 19t-.712-.288T11 18z"
+                />
+              </svg>
 
-            Add
-          </button>
+              Add
+            </button>
+          </div>
+
+          <div class="w-full">
+            <div class="w-full py-5">
+              <ul class="w-full space-y-5">
+                <Todo :todo="{ content: 'Code challenges' }" />
+                <Todo :todo="{ content: 'Code challenges' }" />
+                <Todo :todo="{ content: 'Code challenges' }" />
+              </ul>
+              <p class="text-center">It's empty here</p>
+            </div>
+          </div>
         </div>
-
-        <div class="w-full border my-4"></div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
-
-<style scoped></style>
+<script setup lang="ts">
+import Todo from "~/components/Todo.vue";
+</script>
