c0c2fee1d7a1cf512efda7a78ee17a652b177fba
Kongbi-Zion <kongbizion3@gmail.com>
2024-04-27 10:51:40 +0100

    project initialization

---
+++ components/NavBar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-full flex justify-between items-center px-4 h-16 border-b">
+    <NuxtLink to="/" class="font-semibold">Home</NuxtLink>
+    <button
+      class="bg-[#047D95] text-white font-semibold rounded-full px-5 py-2"
+    >
+      <p class="text-sm">Sign <span>In</span></p>
+    </button>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="h-screen w-full">
+    <NavBar />
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import NavBar from "@/components/NavBar.vue";
+</script>
